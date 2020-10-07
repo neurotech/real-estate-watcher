70832f1b3a510499672715038d9b35aa5c00e539
Tim Douglas <tim.douglas@phocassoftware.com>
2020-10-07 12:15:54 +1100

    Fix failing test

--- test/index.js
+++ test/index.js
@@ -3,8 +3,6 @@ const test = require("tape");
 const filterAndMapListings = require("../filterAndMapListings.js");
 const generateDiscordMessage = require("../generateDiscordMessage.js");
 
-test.createStream().pipe(tapSpec()).pipe(process.stdout);
-
 test("generateDiscordMessage - ", function (t) {
   t.plan(1);
   let thing = true;
