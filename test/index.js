3ecb2f7fb1179f031620ce2a38dfb882c8d4e398
Tim Douglas <tim.douglas@phocassoftware.com>
2020-10-07 12:14:41 +1100

    Removed tap-spec-emoji

--- test/index.js
+++ test/index.js
@@ -1,9 +1,11 @@
 const test = require("tape");
-const tapSpec = require("tap-spec-emoji");
+
+const filterAndMapListings = require("../filterAndMapListings.js");
+const generateDiscordMessage = require("../generateDiscordMessage.js");
 
 test.createStream().pipe(tapSpec()).pipe(process.stdout);
 
-test("fake test", function (t) {
+test("generateDiscordMessage - ", function (t) {
   t.plan(1);
   let thing = true;
   t.true(thing);
